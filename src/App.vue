95c0d00f1345750b9850dc8e39cfcf5943c67cd5
hvrazel <vrazel97@yahoo.com>
2023-07-28 15:20:50 -0500

    fixing frontend issues

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,63 @@
 <template>
-  <div class="app">
-    <div class="categories">
-      <button class="create-category-button" @click="createCategory">Create Category</button>
-      <button class="delete-category-button" @click="deleteCategory">Delete Category</button>
-      <div class="category-list">
-        <div
-          v-for="(category, index) in categories"
-          :key="index"
-          class="category-item"
-          :class="{active: selectedCategory === index}"
-          @click="selectCategory(index)"
-        >
-          {{ category }}
+  <div id="container">
+    <div id="titleDiv">
+      <h1>Dash Video Gallery</h1>
+    </div>
+    <div id="videoListDiv">
+      <div id="categoryDiv" class="bordered small-div">
+        <h3>Categories</h3>
+        <div v-if="categories.length === 0">
+          <button @click="createCategory">Create Category</button>
+        </div>
+        <div v-else>
+          <button @click="createCategory" class="category-button">Create</button>
+          <button @click="deleteCategory" class="category-button">Delete</button>
+          <ul>
+            <li v-for="(category, index) in categories" :key="index" @click="selectCategory(index)" :class="{'selected-category': selectedCategory === index}">{{category}}</li>
+          </ul>
         </div>
       </div>
-    </div>
-    <div class="videos">
-      <div class="video-list">
-        <div
-          v-for="video in videos"
-          :key="video.filename"
-          class="video-item"
-          @click="toggleVideoSelection(video)"
-          :class="{active: isSelected(video)}"
-        >
-          {{ video.filename }}
+      <div id="videoDiv" class="bordered large-div">
+        <div id="videoTitleDiv">
+          <h2>Uploaded Videos:</h2>
         </div>
-      </div>
-      <div class="upload">
-        <input type="file" @change="handleFileSelection">
-        <button @click="handleUpload">Upload</button>
-        <div class="upload-error" v-if="uploadError">{{ uploadError }}</div>
+        <ul>
+          <li class="video-item" v-for="video in videos" :key="video.filename">
+            <span @click="toggleVideoSelection(video)">{{video.filename}}</span>
+            <input type="checkbox" :checked="isSelected(video)" @change="toggleVideoSelection(video)">
+            <button @click="removeVideo(video)" v-if="isSelected(video)">Remove</button>
+          </li>
+        </ul>
       </div>
     </div>
-    <div class="selected-videos">
-      <h3>Selected Videos</h3>
+    <div id="spacer"></div>
+    <div id="uploadDiv">
+      <form @submit.prevent="handleUpload">
+        <input type="file" ref="fileInput" id="file" accept="video/*" hidden @change="handleFileSelection"/>
+        <label for="file" class="upload-button">Choose File</label>
+        <button type="submit" class="upload-button">Upload</button>
+      </form>
+    </div>
+    <div v-if="uploadError" id="uploadError" class="error">
+      <p class="alert-text">{{ uploadError }}</p>
+    </div>
+    <div id="selectedVideosDiv" v-if="selectedVideos.length > 0" class="bordered selected-videos">
+      <h2>Selected Videos:</h2>
       <p>{{ selectedVideos.map(video => video.filename).join(', ') }}</p>
     </div>
-    <div class="email-container">
-      <div class="button-wrapper" v-if="!showEmailForm">
-        <button @click="prepareEmail">Prepare Email</button>
-      </div>
-      <div class="email-form" v-else>
-        <div class="email-field">
-          <label for="recipientEmail">Recipient Email:</label>
-          <input id="recipientEmail" type="text" v-model="recipientEmail" placeholder="Recipient Email">
-        </div>
-        <div class="email-field">
-          <label for="emailSubject">Subject:</label>
-          <input id="emailSubject" type="text" v-model="emailSubject" placeholder="Subject">
-        </div>
-        <div class="email-field">
-          <label for="emailBody">Email Body:</label>
-          <textarea id="emailBody" v-model="emailBody" placeholder="Email Body"></textarea>
-        </div>
-        <div class="button-wrapper">
-          <button @click="sendEmail">Send</button>
-          <button @click="cancelEmail">Cancel</button>
-        </div>
-      </div>
+    <div id="prepareEmailDiv" v-if="!showEmailForm && selectedVideos.length > 0">
+      <button @click="prepareEmail" class="prepareButton">Prepare Email</button>
+    </div>
+    <div id="emailFormDiv" v-if="showEmailForm" class="bordered email-form">
+      <h2>Prepare Email:</h2>
+      <label for="recipientEmail">Recipient:</label>
+      <input type="email" id="recipientEmail" v-model="recipientEmail" required>
+      <label for="emailSubject">Subject:</label>
+      <input type="text" id="emailSubject" v-model="emailSubject" required>
+      <label for="emailBody">Body:</label>
+      <textarea id="emailBody" v-model="emailBody" required></textarea>
+      <button @click="cancelEmail" class="cancelButton">Cancel</button>
+      <button @click="sendEmail" class="sendButton">Send</button>
     </div>
   </div>
 </template>
@@ -233,7 +232,7 @@ body, html {
   padding: 0;
 }
 
-.app {
+#container {
   display: flex;
   flex-direction: column;
   border: 3px solid black;
@@ -243,97 +242,131 @@ body, html {
   overflow: hidden;
 }
 
-.categories {
+#titleDiv {
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  height: 10%;
+}
+
+#videoListDiv {
+  display: flex;
+  justify-content: space-between;
+  height: 45%;
   padding: 20px;
+  background-color: white;
 }
 
-.create-category-button,
-.delete-category-button {
-  margin-bottom: 10px;
-}
-
-.category-list {
+#categoryDiv, #videoDiv {
   display: flex;
   flex-direction: column;
   overflow-y: auto;
-}
-
-.category-item {
-  cursor: pointer;
+  border: 1px solid black;
   padding: 10px;
+  margin-top: 20px;
+  align-items: center;
 }
 
-.category-item.active {
-  font-weight: bold;
-  text-decoration: underline;
+#spacer {
+  flex-grow: .15;
+  background-color: white;
 }
 
-.videos {
+.small-div {
+  width: 30%;
+  height: 100%;
+}
+
+.large-div {
+  width: 65%;
+  height: 100%;
+}
+
+#uploadDiv, #sendDiv {
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 20px;
 }
 
-.video-list {
-  display: flex;
-  flex-direction: column;
-  overflow-y: auto;
+#uploadDiv {
+  height: 10%;
 }
 
-.video-item {
-  cursor: pointer;
-  padding: 10px;
-}
-
-.video-item.active {
-  font-weight: bold;
-  text-decoration: underline;
-}
-
-.upload {
+#sendDiv {
+  height: 10%;
   margin-top: 20px;
 }
 
-.upload-error {
-  color: red;
+.category-button {
+  margin-bottom: 5px;
+  margin-right: 5px;
 }
 
-.selected-videos {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
+.bordered {
+  margin-bottom: 5px;
+  border: 1px solid black;
+  padding: 10px;
 }
 
-.email-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
+.active {
+  font-weight: bold;
+  text-decoration: underline;
 }
 
 .button-wrapper {
-  margin-top: 20px;
-}
-
-.email-form {
   display: flex;
-  flex-direction: column;
+  justify-content: flex-start;
 }
 
-.email-field {
-  margin-bottom: 10px;
+.action-button {
+  margin-right: 5px;
 }
+
+.selected-category {
+  font-weight: bold;
+  text-decoration: underline;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.upload-button, .sendButton {
+  display: inline-block;
+  padding: 10px 24px;
+  margin-bottom: 0;
+  font-size: 14px;
+  color: #333;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  white-space: nowrap;
+  background-color: #fff;
+  text-decoration: none;
+  margin-top: 45px;
+}
+
+.upload-button {
+  margin-right: 10px;
+}
+
+.upload-button:hover, .sendButton:hover {
+  background-color: #d3d3d3;
+}
+
+.alert-text {
+  text-align: center;
+}
+
+.selected-videos {
+  height: 150px;
+  overflow-y: auto;
+  background: white;
+}
+
 </style>
-
 
 
 
